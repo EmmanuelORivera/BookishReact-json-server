87c442b6a12a186ce96c4290b28c506c5d290f79
Emmanuel Ochoa <eorivera95@gmail.com>
2021-09-30 12:45:42 -0600

    adding cors to the json-server

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const jsonServer = require("json-server");
+const cors = require("cors");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
+
+server.use(cors());
 
 server.use((req, res, next) => {
   if (req.method === "DELETE" && req.query["_cleanup"]) {
