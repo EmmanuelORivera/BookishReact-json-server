1f84cc83001d56d60e54b4a586668cc273ac0a23
Emmanuel Ochoa <eorivera95@gmail.com>
2021-09-30 12:04:15 -0600

    middleware to clean db

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
-const jsonServer = require('json-server');
+const jsonServer = require("json-server");
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
+const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
+
+server.use((req, res, next) => {
+  if (req.method === "DELETE" && req.query["_cleanup"]) {
+    const db = router.db;
+    db.set("books", []).write();
+    res.sendStatus(204);
+  } else {
+    next();
+  }
+});
 
 server.use(middlewares);
 server.use(router);
